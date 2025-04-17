4a1af75f85b7e2ec008d24b95bba67baa4557e24
Evhen Ivanov <30300100+Evgiv08@users.noreply.github.com>
2025-04-17 10:17:18 +0000

    fix

--- firebase-config.js
+++ firebase-config.js
@@ -4,8 +4,9 @@ import { getDatabase, ref, onValue, push, set, remove, update } from "https://ww
 const firebaseConfig = {
     apiKey: "AIzaSyCXVaKdHMT__FXNFQsJ3ER4Ss-sHOWvbVQ",
     authDomain: "shoping-list-real.firebaseapp.com",
+    databaseURL: "https://shoping-list-real-default-rtdb.europe-west1.firebasedatabase.app",
     projectId: "shoping-list-real",
-    storageBucket: "shoping-list-real.firebasestorage.app",
+    storageBucket: "shoping-list-real.appspot.com",
     messagingSenderId: "266818025489",
     appId: "1:266818025489:web:f1f1f057d92571bb1999b0",
     measurementId: "G-FKS4M2V6NV"
